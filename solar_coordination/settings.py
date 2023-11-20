6095d5686a9f3a5fddddf7c4f2418552624ecb95
SoniaNkt <soniankatha@gmail.com>
2023-11-20 11:38:30 +0000

    removing ignored files + adding login functionality

--- solar_coordination/settings.py
+++ solar_coordination/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+import os, socket
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -56,7 +57,7 @@ TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
         "DIRS": [
-            # BASE DIR / "templates"
+            # BASE DIR / "solar_coordination_classic" / "templates",
         ],
         "APP_DIRS": True,
         "OPTIONS": {
@@ -124,3 +125,6 @@ STATIC_URL = "static/"
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+LOGIN_REDIRECT_URL = "/solar_coordination_classic/"
+LOGOUT_REDIRECT_URL = "/solar_coordination_classic/"
