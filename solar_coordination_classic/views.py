59fa78cdcb9bbea789a02e8b6f871c4120a3aa45
SoniaNkt <soniankatha@gmail.com>
2023-11-20 11:52:35 +0000

    Require login + provide logout functionlaity for overview

--- solar_coordination_classic/views.py
+++ solar_coordination_classic/views.py
@@ -1,13 +1,16 @@
 from django.shortcuts import render
 from django.http import Http404
+from django.contrib.auth.decorators import login_required
 # Create your views here.
 
+@login_required
 def index(request):
     try:
         return render(request, "solar_coordination_classic/index.html")
     except:
         raise Http404()
 
+@login_required
 def overview(request):
     try:
         solar_values = [0,0,0,0,0,10,40,53,60,100,90,72,50,11,0,0,0,0,0]
